a75429831af7a17b59c5e34c8aea90cc121e0988
Michael Mikhailov <yohanson@users.noreply.github.com>
2021-09-06 02:01:27 +0700

    Tune PID
    15 cycles. Nozzle @ 210 and bed @ 75 degrees.

--- Configuration.h
+++ Configuration.h
@@ -579,9 +579,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  12.62
+    #define DEFAULT_Ki  0.60
+    #define DEFAULT_Kd  66.19
   #endif
 #endif // PIDTEMP
 
@@ -620,9 +620,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 245.07
+  #define DEFAULT_bedKi 42.74
+  #define DEFAULT_bedKd 936.89
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
