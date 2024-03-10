dafcc623bfd4e9c46a51c7865d703ffabf699a13
Michael Mikhailov <yohanson@users.noreply.github.com>
2024-03-10 18:01:29 +0700

    Set boot splash to 1.5s

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -1437,7 +1437,7 @@
   //#define LCD_TIMEOUT_TO_STATUS 15000   // (ms)
 
   #if ENABLED(SHOW_BOOTSCREEN)
-    #define BOOTSCREEN_TIMEOUT 3000       // (ms) Total Duration to display the boot screen(s)
+    #define BOOTSCREEN_TIMEOUT 1500       // (ms) Total Duration to display the boot screen(s)
     #if EITHER(HAS_MARLINUI_U8GLIB, TFT_COLOR_UI)
       #define BOOT_MARLIN_LOGO_SMALL      // Show a smaller Marlin logo on the Boot Screen (saving lots of flash)
     #endif
